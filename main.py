a2761f48ab3611513662b728028eec38c77da14d
NannNann-bjyx <nanneihtun@gmail.com>
2026-09-12 17:18:29 +0200

    hello-world-change

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from dotenv import load_dotenv
 from langchain_core.prompts import PromptTemplate
 from langchain_openai import ChatOpenAI
+from langchain_ollama import ChatOllama
+
 load_dotenv()
 
 def main():
@@ -28,6 +30,8 @@ Musk is a supporter of global far-right politics, figures, and political parties
         )
 
     llm = ChatOpenAI(temperature=0, model="gpt-5")
+    #llm = ChatOllama(temperature=0, model="gemma3:270m")
+    
     chain = summary_prompt_template | llm
 
     response = chain.invoke(input={"information": information})
